bb5ce1546fc9b89c0c1d6ce0ddde9c23c622de54
ZERO <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-05-16 02:19:19 +0700

    change bot info on decorators

--- tg_bot/modules/anilist.py
+++ tg_bot/modules/anilist.py
@@ -12,7 +12,7 @@ from telegram import (
 import requests
 import math
 import time
-from tg_bot.modules.helper_funcs.decorators import kigcmd
+from tg_bot.modules.helper_funcs.decorators import keicmd
 
 def shorten(description, info="anilist.co"):
     msg = ""
@@ -160,7 +160,7 @@ query ($id: Int,$search: String) {
 
 url = "https://graphql.anilist.co"
 
-@kigcmd(command="airing")
+@keicmd(command="airing")
 def airing(update: Update, context: CallbackContext):
     message = update.effective_message
     search_str = message.text.split(" ", 1)
@@ -182,7 +182,7 @@ def airing(update: Update, context: CallbackContext):
         msg += f"\n*Episode*:{response['episodes']}\n*Status*: `N/A`"
     update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
 
-@kigcmd(command="anime")
+@keicmd(command="anime")
 def anime(update: Update, context: CallbackContext):
     message = update.effective_message
     search = message.text.split(" ", 1)
@@ -255,7 +255,7 @@ def anime(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
 
-@kigcmd(command="character")
+@keicmd(command="character")
 def character(update: Update, context: CallbackContext):
     message = update.effective_message
     search = message.text.split(" ", 1)
@@ -289,7 +289,7 @@ def character(update: Update, context: CallbackContext):
                 msg.replace("<b>", "</b>"), parse_mode=ParseMode.MARKDOWN
             )
 
-@kigcmd(command="manga")
+@keicmd(command="manga")
 def manga(update: Update, context: CallbackContext):
     message = update.effective_message
     search = message.text.split(" ", 1)
